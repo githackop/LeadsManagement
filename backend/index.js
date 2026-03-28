7a9c0c4d9466a9245c69b6a493e15c6aedc869ef
githackop <uppalapatiroshini18@gmail.com>
2026-03-28 16:22:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // ── Middleware ──────────────────────────────────────────────
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['http://localhost:3000', 'http://localhost:5173','https://leads-management-frontend-ten.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
